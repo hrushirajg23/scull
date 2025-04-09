73d49ce0d8ec0dc23c718b922a123e5e955ae4c6
hrushirajg23 <hrushirajg23@gmail.com>
2025-04-09 22:59:02 +0530

    write process update, some bug fixes too

--- scullpipe.c
+++ scullpipe.c
@@ -31,7 +31,7 @@ struct scull_pipe{
 static ssize_t scull_p_read(struct file* filp,char __user* buf,size_t count,loff_t* fpos){
      struct scull_pipe* pdev=filp->private_data;
 
-     if(down_interruptible(&dev->sem))
+     if(down_interruptible(&pdev->sem))
           return -ERESTARTSYS;
      /*
           pipe empty
@@ -62,7 +62,7 @@ static ssize_t scull_p_read(struct file* filp,char __user* buf,size_t count,loff
           /*buffer full lihun zhalay mhanun, write parat 0 la gela ahe*/
           count=MIN(count,(size_t)(pdev->end-pdev->rp));
      }
-     if(copy_to_user(buf,dev->rp,count)){
+     if(copy_to_user(buf,pdev->rp,count)){
         
           up(&pdev->sem);
           return -EFAULT;  
@@ -82,6 +82,62 @@ static ssize_t scull_p_read(struct file* filp,char __user* buf,size_t count,loff
      */
      wake_up_interruptible(&pdev->outq);
      PDEBUG("\"%s\" did read %li bytes\n",current->comm,(long)count);
+     return count;
+
+}
+
+
+static ssize_t scull_p_write(struct file* filp,char __user* buf,size_t count,loff_t* fpos){
+     struct scull_pipe* pdev=filp->private_data;
+
+     if(down_interruptible(&pdev->sem))
+          return -ERESTARTSYS;
+     /*
+          pipe empty
+     */
+   
+
+     // while(pdev->rp==pdev->wp){
+     //      up(&pdev->sem);
+     //      if(filp->flags & O_NONBLOCK){
+     //           return -EAGAIN;
+     //      }
+     //      PDEBUG("\"%s\" reading: going to sleep\n",current->comm);
+     //      if(wait_event_interruptible(pdev->inq,(pdev->rp!=pdev->wp)))
+     //           return -ESTARTSYS;
+     //      if(down_interruptible(&pdev->sem)){ /*havrat pana nako , check again, it may have been acquired by another process*/
+     //           /*
+     //                remember getblk() continue scenario
+     //           */
+               
+     //           return -ERESTARTSYS}
+     // }
+     /* 
+          something present
+          mhanje write jevdha pudhe tevdha read karayche
+          */
+   
+     count=MIN(count,(size_t)(pdev->end-pdev->wp));
+     if(copy_from_user(pdev->rp,buf,count)){
+        
+          up(&pdev->sem);
+          return -EFAULT;  
+     }
+     pdev->wp+=count;
+     if(pdev->wp==pdev->end){
+          /*
+               if read has reached end move it to start again
+          */
+          pdev->wp=pdev->buffer; 
+          
+     }
+     up(&pdev->sem);
+
+     /*
+          wakeup the writers
+     */
+     wake_up_interruptible(&pdev->inq);
+     PDEBUG("\"%s\" did write %li bytes\n",current->comm,(long)count);
      return count;
 
 }
